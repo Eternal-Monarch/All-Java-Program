9735c13ebe6f2f6812523e9113fcb1c0e51cad08
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-04-02 08:55:30 +0600

    Create 3.Format specifier.java

---
+++ 3.Format specifier.java
@@ -0,0 +1,34 @@
+public class Main
+{
+  
+ 
+ 
+public static void main (String[]args)
+  {
+    
+boolean b = true;
+    
+char c = 'a';
+    
+short s = 326;
+    
+int i = 89;
+    
+float f = 8.2f;
+    
+double d = 10.2;
+    
+      //dynamic initialiazation
+      
+System.out.printf ("b = %b\n", b);
+    
+System.out.printf ("c =%c\n", c);
+    
+System.out.printf ("s =%s\n", s);
+    
+System.out.printf ("i =%d\n", i);
+System.out.printf ("f =%f\n", f);
+System.out.printf ("d =%f\n", d);
+} 
+ 
+} 
