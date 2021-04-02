e45854908f28a823236d6ec1bb6fa2dc6af2290c
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-04-02 09:07:52 +0600

    Create 4.How to get user input.java

---
+++ 4.How to get user input.java
@@ -0,0 +1,25 @@
+package how.to.get.user.input;
+
+import java.util.Scanner;
+
+public class HowToGetUserInput
+{
+  
+ 
+ 
+public static void main (String[]args)
+  {
+    
+int i = 120;		//dynamic initiliazation
+    Scanner input = new Scanner (System.in) 
+      System.out.println ("Enter the number : ");
+    
+      //store the value from the taken by the user
+      i = input.nextInt ();
+    2 
+ System.out.println ("Number is " + i);
+
+ 
+} 
+ 
+} 
