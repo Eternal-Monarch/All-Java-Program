ce8e015e509ce3949bddac8479bb0bb1d98861db
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-04-02 06:02:27 +0600

    Create Print Hello World In Java.java

---
+++ Print Hello World In Java.java
@@ -0,0 +1,32 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package first.java.program;
+
+/**
+ *
+ * @author Shuvo
+ */
+public class FirstJAvaProgram {
+
+    /**
+     * @param args the command line arguments
+     */
+    //main is the fucntion
+    //public static void  are the keyword
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //print is the metho or function
+        System.out.println("Hello World\n");
+      
+        //; semeicolon is the statement 
+        System.out.println("Computer Science & Engineering");
+        System.out.println("1\t 2");
+        //Escape Sequnece 
+        System.out.println("3\t 4");
+        
+    }
+    
+}
