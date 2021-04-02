c84a275628f39d2b52c6b1ce636ea771ba8ca64c
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-04-02 08:35:51 +0600

    Create 2.Variable and data type in java.java

---
+++ 2.Variable and data type in java.java
@@ -0,0 +1,27 @@
+
+
+
+
+public class Main {
+
+
+    public static void main(String[] args) {
+      boolean b=true;
+      char c='a';
+      short s = 326;
+      int i  = 89;
+      float f = 8.2f;
+      double d=10.2;//dynamic initialiazation
+      
+      System.out.println("b = "+b);
+      System.out.println("c = "+c);
+      System.out.println("s = "+s);
+      System.out.println("f = "+f);
+      
+      
+      
+ 
+             
+    }
+    
+}
