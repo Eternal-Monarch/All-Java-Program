97c16740de17ac0ca2384787c24a2c10bf4a93ea
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-08-09 10:22:00 +0600

    Create do while.java

---
+++ do while.java
@@ -0,0 +1,17 @@
+
+public class Main
+{
+  public static void main (String[]args)
+  { 
+      int counter=1;
+      
+      while(counter<101)
+      {
+          System.out.println(counter);
+          counter++;
+          
+      }
+   
+  }
+
+}
