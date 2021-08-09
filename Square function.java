9b6b74cdb2cc132b1ad10cac2e0e9ca28044e983
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-08-09 10:17:41 +0600

    Create Square function.java

---
+++ Square function.java
@@ -0,0 +1,16 @@
+
+public class Main
+{
+  public static void main (String[]args)
+  {
+square(10);
+    
+  }
+ static void square(int number)
+  {
+      int square = number*number;
+      System.out.println ("the value is "+square);
+      //return square;
+  }
+
+}
