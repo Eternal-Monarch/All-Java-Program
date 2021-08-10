eb72f0b066527bd0b0a49578b1ee975467882ac7
Subhasish Dutta <58630896+Eternal-Monarch@users.noreply.github.com>
2021-08-10 07:39:40 +0600

    Update if-else.java

--- if-else.java
+++ if-else.java
@@ -1,31 +1,58 @@
 import java.util.Scanner;
-public class Main{
-public static void main(String[] args) {
-    Scanner myScanner =new Scanner(System.in);
-    print("Enter Your Marks: ");
-    int marks=myScanner.nextInt();
-    if(marks>=80)
-    {
-        print("you got A+");
-}
-else if(marks>=70)
-{
-print("you got A");
-}
-else if(marks>=60)
-{
-    print("you got A-");
-}
-else if(marks<=40)
-{
-    print("you got failed!!");
-}
-}
-public static void print(String msg)
-{
-    System.out.println(msg);
 
-}
-
+public class Main
+{
+  
+public static void main (String[]args)
+  {
     
+Scanner myScanner = new Scanner (System.in);
+    
+print ("Enter Your Marks: ");
+    
+int marks = myScanner.nextInt ();
+    
+if (marks >= 80)
+      
+      {
+	
+print ("you got A+");
+      
 }
+    
+    else if (marks >= 70)
+      
+      {
+	
+print ("you got A");
+      
+}
+    
+    else if (marks >= 60)
+      
+      {
+	
+print ("you got A-");
+      
+}
+    
+    else if (marks <= 40)
+      
+      {
+	
+print ("you got failed!!");
+      
+}
+  
+}
+  
+public static void print (String msg) 
+  {
+    
+System.out.println (msg);
+
+ 
+} 
+ 
+ 
+} 
